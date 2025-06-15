20777e29a2d06ddc472d79087dc4d3b1ab825486
Arminas03 <arminas.raz@gmail.com>
2025-06-15 17:16:34 +0200

    improve xgboost, try pca

--- run.py
+++ run.py
@@ -2,3 +2,7 @@ from models.run_xgb import main
 
 if __name__ == "__main__":
     main()
+    # data = pd.read_parquet("data/train.parquet")
+    # n_row = data.shape[0]
+    # data = data.iloc[math.floor(n_row * 0.95) :, :]
+    # data.to_parquet("data/train_reduced.parquet")
--- scripts/data.py
+++ scripts/data.py
@@ -5,8 +5,8 @@ def drop_const_columns(df: pd.DataFrame):
     df.drop(columns=[col for col in df.columns if df[col].nunique() == 1], inplace=True)
 
 
-def get_data():
-    data = pd.read_parquet("data/train.parquet")
+def get_data(path="data/train_reduced.parquet"):
+    data = pd.read_parquet(path)
 
     drop_const_columns(data)
 
